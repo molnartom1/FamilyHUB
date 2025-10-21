fd3c8cf1b6af807f321a8e7a90dd4be766f1f36a
molnartom1 <molnar.tamas7@icloud.com>
2025-10-21 12:38:19 +0200

    Delete src directory

--- src/lib/date.ts
+++
@@ -1,8 +0,0 @@
-export const todayStr = () => new Date().toISOString().slice(0,10)
-export const addDays = (d: Date, n: number) => new Date(d.getTime() + n*86400000)
-export const withinNextDays = (dateStr: string, days=7) => {
-  const target = new Date(dateStr + "T00:00:00")
-  const now = new Date()
-  const diff = (target.getTime() - (new Date(now.toDateString())).getTime())/86400000
-  return diff >= 0 && diff <= days
-}
